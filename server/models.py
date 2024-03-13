ccbc6235935acb6726c460eadc78e02cbbf1b3b8
Jim Grimes <jimgrimes86@gmail.com>
2024-03-12 21:39:11 -0400

    Create server files, pipfile, and basic User model

---
+++ server/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+
+from config import db
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    email = db.Column(db.Text, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.id}: {self.name}>"
+
