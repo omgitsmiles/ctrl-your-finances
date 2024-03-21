0d231c127786da08d7d1aa84d87a7f586557caf2
amanda vikdal <vikdal.amanda@gmail.com>
2024-03-21 09:13:27 -0400

    incorportating fetch still need to access user db

--- client/src/components/Budgeting.jsx
+++ client/src/components/Budgeting.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Chart } from "react-google-charts";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
@@ -7,15 +7,26 @@ import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
 
 
 
+// grab user data and have house account toggle for different 
 
 function Budgeting() {
     const [goalName, setGoalName] = useState("");
     const [savedMoney, setSavedMoney] = useState(0);
     const [targetAmount, setTargetAmount] = useState(0);
+
+    useEffect(() => {
+      // fetch user's goals from backend
+      fetchUserGoals();
+    },[])
+
+    const fetchUserGoals = async () => {
+      try {
+        
+      }
+    }
 
 
     const [data, setData] = useState({
@@ -58,66 +69,20 @@ function Budgeting() {
 
     return (
         <div>
+                <Box
+        sx={{
+        padding: '20px', 
+        border: '1px solid #ccc', // Add border to create a box effect
+        borderRadius: '5px', // Add border radius for rounded corners
+        maxWidth: '500px', // Limit the width of the box
+        // marginRight: 'auto', // Center the box horizontally
+        }}
+      >
             <h1 style={{fontFamily: 'Poppins', color: '#009933'}}>BUDGETING GOALS</h1>
             <h2 style={{ fontFamily: 'Poppins', color: '#32004C' }}>Set Your Financial Goals</h2>
             <p style={{ fontFamily: 'Poppins', color: '#32004C' }}>Start your journey towards financial well-being today. Define clear, achievable goals that align with your aspirations and values.</p>
 
-            {/* <Box
-                component="form"
-                sx={{
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                    border: '1px solid #ccc',
-                }}
-                noValidate
-                autoComplete="off"
-                onSubmit={handleSubmit}
-            >
-
-                <TextField
-                    label="What are you saving for"
-                    id="outlined-start-adornment"
-                    sx={{ m: 1, width: '25ch' }}
-                    type="text"
-                    value={goalName}
-                    onChange={(e) => setGoalName(e.target.value)}
-                />
-    
-        <FormControl  sx={{ m: 1 }}>
-          <InputLabel htmlFor="outlined-adornment-amount">Saved </InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-amount"
-            startAdornment={<InputAdornment position="start">$</InputAdornment>}
-            label="Amount"
-            type="number"
-            value={savedMoney}
-            onChange={(e) => setSavedMoney(parseInt(e.target.value))}
-          />
-        </FormControl>
-       
-        <FormControl  sx={{ m: 1 }}>
-          <InputLabel htmlFor="outlined-adornment-amount">Target </InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-amount"
-            startAdornment={<InputAdornment position="start">$</InputAdornment>}
-            label="Amount"
-            type="number"
-            value={targetAmount}
-            onChange={(e) => setTargetAmount(parseInt(e.target.value))}
-          />
-        </FormControl>
-        <Button type="submit" variant="contained" size="large" style={{ backgroundColor: "#009933" }}>Add Goal</Button>
-    
-      </Box> */}
-      <Box
-    sx={{
-        padding: '20px', // Adjust padding as needed
-        border: '1px solid #ccc', // Add border to create a box effect
-        borderRadius: '5px', // Add border radius for rounded corners
-        maxWidth: '500px', // Limit the width of the box
-        marginLeft: 'auto', // Center the box horizontally
-        marginRight: 'auto', // Center the box horizontally
-    }}
->
+        
     <form
         noValidate
         autoComplete="off"
@@ -126,6 +91,7 @@ function Budgeting() {
     >
         <TextField
             label="What are you saving for"
+            fullWidth
             id="outlined-start-adornment"
             sx={{ m: 1, width: '100%', display: 'block' }} 
             type="text"
@@ -139,16 +105,18 @@ function Budgeting() {
                 id="outlined-adornment-amount"
                 startAdornment={<InputAdornment position="start">$</InputAdornment>}
                 label="Amount"
+                fullWidth
                 type="number"
                 value={savedMoney}
                 onChange={(e) => setSavedMoney(parseInt(e.target.value))}
             />
         </FormControl>
 
-        <FormControl sx={{ m: 1, width: '100%', display: 'block' }}> 
+        <FormControl sx={{ m: 1, width: '100%', display: 'block' }} > 
             <InputLabel htmlFor="outlined-adornment-amount">Target</InputLabel>
             <OutlinedInput
                 id="outlined-adornment-amount"
+                fullWidth
                 startAdornment={<InputAdornment position="start">$</InputAdornment>}
                 label="Amount"
                 type="number"
