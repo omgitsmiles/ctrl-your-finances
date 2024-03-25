1c7ae22b8acc3348850f839f6747aeea8e618955
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2024-03-25 12:01:21 -0400

    added user.displayName to get users initial on login

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -115,7 +115,7 @@ function ResponsiveAppBar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Paolo Alberca" src="/static/images/avatar/2.jpg" />
+                <Avatar alt={user?.displayName} src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
